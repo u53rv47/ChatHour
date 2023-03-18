71290e6438810a674ef8e10cf104c577da956251
u53rv47 <vijaysingh997022@gmail.com>
2023-03-18 12:13:02 +0530

    updated authorizer

--- src/chatHourAuthorizer/app.py
+++ src/chatHourAuthorizer/app.py
@@ -1,45 +1,16 @@
-from jose.utils import base64url_decode
-from jose import jwk, jwt
-import urllib.request
-import time
 import os
 import json
-import boto3
+import time
+import urllib.request
+from jose import jwk, jwt
+from jose.utils import base64url_decode
 
-
-# import requests
-cognito_client = boto3.client('cognito-idp')
-
-
-def lambda_handler(event, context):
-    event['headers']['authorizer']
-    username = event['headers']['username']
-    password = event['headers']['password']
-
-    cognito_client.admin_initiate_auth(
-        UserPoolId=os.environ['USER_POOL_ID'],
-        ClientId=os.environ['CLIENT_ID'],
-        AuthFlow='ADMIN_NO_SRP_AUTH',
-        AuthParameters={
-            'USERNAME': username,
-            'PASSWORD': password
-        }
-    )
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
-
-
-region = 'ap-southeast-2'
-userpool_id = 'ap-southeast-2_xxxxxxxxx'
-app_client_id = '<ENTER APP CLIENT ID HERE>'
+region = os.environ['REGION']
+userpool_id = os.environ['USER_POOL_ID']
+app_client_id = os.environ['CLIENT_ID']
 keys_url = 'https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.json'.format(
     region, userpool_id)
+
 # instead of re-downloading the public keys every time
 # we download them only on cold start
 # https://aws.amazon.com/blogs/compute/container-reuse-in-lambda/
@@ -60,7 +31,7 @@ def lambda_handler(event, context):
             key_index = i
             break
     if key_index == -1:
-        print('Public key not found in jwks.json')
+        # print('Public key not found in jwks.json')
         return False
     # construct the public key
     public_key = jwk.construct(keys[key_index])
@@ -71,28 +42,20 @@ def lambda_handler(event, context):
     decoded_signature = base64url_decode(encoded_signature.encode('utf-8'))
     # verify the signature
     if not public_key.verify(message.encode("utf8"), decoded_signature):
-        print('Signature verification failed')
+        # print('Signature verification failed')
         return False
-    print('Signature successfully verified')
+    # print('Signature successfully verified')
     # since we passed the verification, we can now safely
     # use the unverified claims
     claims = jwt.get_unverified_claims(token)
     # additionally we can verify the token expiration
     if time.time() > claims['exp']:
-        print('Token is expired')
+        # print('Token is expired')
         return False
     # and the Audience  (use claims['client_id'] if verifying an access token)
     if claims['aud'] != app_client_id:
-        print('Token was not issued for this audience')
+        # print('Token was not issued for this audience')
         return False
     # now we can use the claims
-    print(claims)
+    # print(claims)
     return claims
-
-
-# the following is useful to make this script executable in both
-# AWS Lambda and any other local environments
-if __name__ == '__main__':
-    # for testing locally you can enter the JWT ID Token here
-    event = {'token': ''}
-    lambda_handler(event, None)
